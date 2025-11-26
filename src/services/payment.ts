2315cd1f31f5b19fad56e61436e67d9869bfabb0
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-11-26 15:23:09 +0700

    update service payment, pass userClerkId as another param

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -1,16 +1,15 @@
 import db from '@/lib/db';
+import { Prisma } from '@prisma/client';
 
-export async function createPaymentByClerkId(data: {
-  userClerkId: string;
-  provider: string;
-  status: 'PENDING' | 'PAID' | 'FAILED' | 'REFUNDED';
-  amount: number;
-  stripe_payment_id?: string;
-}) {
+export async function createPaymentByClerkId(
+  userClerkId: string,
+  data: Omit<Prisma.PaymentDetailsCreateInput, 'userClerkId' | 'user'>
+) {
   return db.paymentDetails.create({
     data: {
       ...data,
-      stripe_payment_id: data.stripe_payment_id || `payment_${Date.now()}`
+      stripe_payment_id: data.stripe_payment_id || `payment_${Date.now()}`,
+      userClerkId: userClerkId
     }
   });
 }
